92b5f453854b0f029ac23c1685b71ad2a6ebde26
Kevin Casey <kacasey@berkeley.edu>
2014-04-24 01:33:49 -0700

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Kevincasey::Application.routes.draw do
   resources :projects
 
+  root to: 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
