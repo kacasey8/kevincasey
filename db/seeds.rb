9809fdec501f7e770438b45804f5f56b93f751c5
Kevin Casey <kacasey@berkeley.edu>
2014-04-23 21:44:54 -0700

    added more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,32 @@
 
 def initialize_projects
   projects = [
-    {name: 'HackFSM', timeframe: '4/1/14 - 4/12/14', blurb: 'Weeklong hackathon hosted by the Bancroft Library at Berkeley.', event_type: 'hackathon', repo: 'https://github.com/craighiller/hackfsm', demo: 'http://fsm-archive.appspot.com', description: 'TEMP'}
+    {
+      name: 'Hack FSM', 
+      timeframe: '4/1/14 - 4/12/14', 
+      blurb: 'Weeklong hackathon hosted by the Bancroft Library at Berkeley. Designed and built a web interface for the FSM Archive', 
+      event_type: 'hackathon', repo: 'https://github.com/craighiller/hackfsm', 
+      demo: 'http://fsm-archive.appspot.com', 
+      description: "Web app written in python using the Bottle framework to do routing.  Our app won first place"
+    },
+    {
+      name: 'Hall Of Time', 
+      timeframe: '3/8/14 - 18 hrs', 
+      blurb: "Blueprint's Social good hackathon. Built a timesheet app for the Lawrence Hall Of Science", 
+      event_type: 'hackathon', 
+      repo: 'https://github.com/Fortisque/halloftime', 
+      demo: 'http://www.calblueprint.org/hackathons/save-the-day-code-for-good/hacks/hall-of-time', 
+      description: 'TEMP'
+    },
+    {
+      name: 'Food Me', 
+      timeframe: '3/1/14 - 12 hrs', 
+      blurb: "Hackers@Berkeley's HackJam. Built a when to meet specifically designed for food", 
+      event_type: 'hackathon', 
+      repo: 'https://github.com/Fortisque/halloftime', 
+      demo: 'http://www.calblueprint.org/hackathons/save-the-day-code-for-good/hacks/hall-of-time', 
+      description: 'TEMP'
+    },
   ]
 
   projects.each do |project|
@@ -16,4 +41,5 @@ def initialize_projects
   end
 end
 
+Project.delete_all # clean up the database in case changes
 initialize_projects
