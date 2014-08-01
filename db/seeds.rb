0a47cfb835dfd9c189b863ac7608e723e85239aa
Kevin Casey <kcasey@godaddy.com>
2014-08-01 16:38:37 -0700

    moved images to assets folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ def initialize_projects
       event_type: 'hackathon', 
       repo: 'https://github.com/skleung/get-matched',
       demo: 'http://get-matched.herokuapp.com/',
-      image_src: '/get-matched.png',
+      image_src: 'get-matched.png',
       description: "We used the Locu api in order to pull information about buisnesses and used a custom algorithm to match up the buisnesses.
               The idea being that we only match buisnesses if both the buisnesses hit accept on each other.  Search for buisnesses by filling in needs and search
               for customers so that both people can accept each other.  Have a custom chat system built in."
@@ -27,7 +27,7 @@ def initialize_projects
       event_type: 'hackathon', 
       repo: 'https://github.com/craighiller/serendipity',
       demo: 'http://hackserendipity.appspot.com/',
-      image_src: '/serendipity.png',
+      image_src: 'serendipity.png',
       description: "We wanted this to be a cute app to encourage people to donate to charity and also help random strangers.  We struggled a lot on this because we
             rolled our own user authentication system (which is also completely insecure, sorry).  We managed to get out features of texting through twillio
             and fairly pleasing UI."
@@ -39,7 +39,7 @@ def initialize_projects
       event_type: 'hackathon', 
       repo: 'https://github.com/craighiller/hackfsm',
       demo: 'http://fsm-archive.appspot.com',
-      image_src: '/fsm.png',
+      image_src: 'fsm.png',
       description: "We managed to provide a clean interface for
             researchers exploring the Free Speech Movement Archive.  We were a team of 3 programmers (myself included) and one humanist.
             One of the programmers and our humanist managed give us brilliant ideas for design and feature requests.  I mainly worked on
@@ -51,9 +51,8 @@ def initialize_projects
       blurb: "Best Nonprofit in Blueprint's Social good hackathon. Built a timesheet app for the Lawrence Hall Of Science. Ruby on Rails",
       event_type: 'hackathon',
       repo: 'https://github.com/Fortisque/halloftime',
-      #demo: 'http://www.calblueprint.org/hackathons/save-the-day-code-for-good/hacks/hall-of-time',
       demo: 'http://halloftime.herokuapp.com/',
-      image_src: '/halloftime.png',
+      image_src: 'halloftime.png',
       description: 'This was a rails app, and I was the only one that knew rails beforehand so I handeled the controller and backend logic.
             Luckily my 3 other teammates were able to build and design a beautiful front-end on top of my database.  We added various features from just
             a basic database time logging including a dashboard that can log current tasks and a calendar view.'
@@ -65,7 +64,7 @@ def initialize_projects
       event_type: 'hackathon',
       repo: 'https://github.com/craighiller/foodme',
       demo: 'http://food-me.appspot.com',
-      image_src: '/foodme.png',
+      image_src: 'foodme.png',
       description: "This was a really basic app and our first attempt at a hackathon.  It was really fun to try to build something on our own.
               Features include logging into Facebook to get access to friends and linking the app to texting.  I worked on the backend and did
               some of the frontend for this."
@@ -80,7 +79,7 @@ def initialize_projects
       event_type: 'school project',
       repo: 'https://github.com/cs169-2013/photo_evidence_gallery',
       demo: 'http://photoevidence.herokuapp.com',
-      image_src: '/photoevidence.png',
+      image_src: 'photoevidence.png',
       description: "This was my first experience building a web app, so it was exciting because none of us knew what we were doing. We were 
             a team of 6 and were connected to our client BAMRU (Bay Area Mountain Rescue Unit) through our CS169 class at Berkeley. Our client
             wanted secure photos in order to share potentially crime sensitive information to each other during a rescure operation. Over the 
@@ -95,7 +94,7 @@ def initialize_projects
       timeframe: 'Spring 2014',
       blurb: "1st place winner in a contest of programming multi-agent competitive capture the flag (food pellet) game",
       event_type: 'school contest',
-      image_src: '/capture_the_flag.png',
+      image_src: 'capture_the_flag.png',
       description: "There were 4 different releases to the contest, making the parameters increasingly difficult.
             The first part was a search problem, agents could not be killed, so the problem was a matter steps required to collect pellets.
             I used brute force randomized improvement with a backing of MST and clusting.  For the 3rd and 4th part we used a smart reflex agent
@@ -107,7 +106,7 @@ def initialize_projects
       timeframe: 'Summer 2013',
       blurb: "1st place in a speed competition for multiplying (A * B = C) matrix using pthreads",
       event_type: 'school contest',
-      image_src: '/matrix_multiplication.png',
+      image_src: 'matrix_multiplication.png',
       description: "Had the fastest algorithm among 140 students for multiplying matricies.  We used techniques including register/cache blocking, SSE instructions and
             multi-threading to get up to 70 GFlops.  The most interesting thing was that we realized a lot of the hard work was done computing edge cases (literally
             the edge of the matrix multiplication) because padding the matricies takes too much time and reusing the cache is much more difficult "
@@ -117,7 +116,7 @@ def initialize_projects
       timeframe: 'Spring 2013',
       blurb: "1st place winner for a contest of playing the game called Network (it's similar to checkers)",
       event_type: 'school contest',
-      image_src: '/network.png',
+      image_src: 'network.png',
       description: "Used concepts of intermediate board evaluation, hash mapping, and depth 5 alpha-beta pruning to win a
             bracket based tournament in a class of 500.  My board evaluation was critical, and made sure to only favor positions
             closer to winning.  I saw the game as a simplified game of chess and was able to program a very strong bot to play the game."
@@ -132,7 +131,7 @@ def initialize_projects
       event_type: 'personal projects',
       repo: "https://github.com/skleung/Community",
       demo: "http://community-app.herokuapp.com/",
-      image_src: '/community.png',
+      image_src: 'community.png',
       description: "My friend thought of the idea for this app and I designed most of the backend for this project.
             Nothing like this really exists currently.  We focus on splitting food cost so we can do it right: people
             attend meals on our site and we track how many times an ingredient is used so we can accurately gauge how
@@ -146,7 +145,7 @@ def initialize_projects
       event_type: 'personal projects',
       repo: "https://github.com/chungs1/hearthstone_counter",
       demo: "http://hearthstonecounter.herokuapp.com/",
-      image_src: '/hearthstonecounter.png',
+      image_src: 'hearthstonecounter.png',
       description: "My friend, a buisness major thought of the idea to log the current meta game of the Blizzard
               card game called Hearthstone.  The admin can add decks, and specify the matchups as which deck counters the other.
               Nonadmins can explore the decks and give a thumbs up or thumbs down based on what they think."
@@ -158,7 +157,7 @@ def initialize_projects
       event_type: 'personal projects',
       repo: "github.com/Fortisque/irrational_mind",
       demo: "http://irrational-mind.herokuapp.com",
-      image_src: '/irrationalmind.png',
+      image_src: 'irrationalmind.png',
       description: "I envisioned this app as a solution to the problem of finding eigenvectors of matricies.  When using an online
             caclulator for this it would always give you back the solution in the form of decimal values, but it was obviously more
             useful and intuitive to be able to see the result as a familar figure such as sqrt(5).  I used a brute force
