1fb4614fbb1479981c87cb73157b4b0330ac56ff
Kevin Casey <kacasey@berkeley.edu>
2014-09-27 15:46:51 -0700

    post up

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Kevincasey::Application.routes.draw do
   resources "contacts", only: [:new, :create]
 
   match '/russell', to: 'pages#eval_for_russell', via: :get, as: :russell
+  match '/russell', to: 'pages#eval_for_russell', via: :post, as: :russell
 
 
   # The priority is based upon order of creation: first created -> highest priority.
